883f6d542c02f0d04308aac6deb62af5af6f149e
Kjasn <iz.kjasn@qq.com>
2025-02-20 16:37:40 +0800

    feat: update logic of letter display

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -1,8 +1,12 @@
-export default function Word(props: { word: string }) {
+type WordProps = {
+    word: string;
+    showLetters: number[];
+};
+export default function Word(props: WordProps) {
     return (
         <section className="word">
             {props.word.split("").map((letter, index) => (
-                <span key={index}>{letter}</span>
+                <span key={index}>{props.showLetters.includes(index) ? letter : ""}</span>
             ))}
         </section>
     );
