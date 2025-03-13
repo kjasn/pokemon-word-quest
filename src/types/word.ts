43644d5ae9011b767abd0e490c4f084bb6d9cd01
Kjasn <iz.kjasn@qq.com>
2025-03-13 22:57:16 +0800

    chore: add Word interface for dictionary data

---
+++ src/types/word.ts
@@ -0,0 +1,8 @@
+/**
+ * Dictionary data sourced from: https://github.com/RealKai42/qwerty-learner
+ * Licensed under GPL-3.0
+ */
+export interface Word {
+    name: string;
+    trans: string[];
+}
