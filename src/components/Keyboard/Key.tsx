eec155d0b96055f7bbfc2651dfe4322e2e24e764
Kjasn <iz.kjasn@qq.com>
2025-03-16 17:47:37 +0800

    reactor: tidy code

--- src/components/Keyboard/Key.tsx
+++ src/components/Keyboard/Key.tsx
@@ -3,16 +3,11 @@ import styles from "./index.module.css";
 interface KeyboardKeyProps {
     letter: string;
     onClick?: () => void;
-    disabled: boolean;
 }
 
 export default function KeyboardKey(props: KeyboardKeyProps) {
     return (
-        <button
-            className={styles["keyboard-key"]}
-            onClick={props.onClick}
-            disabled={props.disabled}
-        >
+        <button className={styles["keyboard-key"]} onClick={props.onClick}>
             {props.letter}
         </button>
     );
